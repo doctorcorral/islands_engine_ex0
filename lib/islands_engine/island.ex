c8c82f031bfd5daba2450699371a5de1c4e83488
Ricardo C <ricardo@suggestic.com>
2018-02-28 15:07:38 -0600

    fix typos

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -1,12 +1,22 @@
-defmodule IslansEngine.Islands do
+defmodule IslandsEngine.Island do
   alias IslandsEngine.{Coordinate, Island}
 
   @enforce_keys [:coordinates, :hit_coordinates]
-  defstruct [:coordinates, :hit_coordinatese]
+  defstruct [:coordinates, :hit_coordinates]
 
-  def new(), do:
-  %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+#  def new(), do:
+#  %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
 
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+	 %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+      do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+      else
+	error -> error
+    end
+  end
+  
   defp offsets(:square), do: {{0,0}, {0,1}, {1,0}, {1,1}}
   defp offsets(:atoll), do: [{0,0}, {0,1}, {1,1}, {2,0}, {2,1}]
   defp offsets(:dor), do: [{0,0}]
