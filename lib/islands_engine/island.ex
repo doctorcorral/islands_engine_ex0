2e31fef1e7af792358c147409f934a283fe4bc8a
Ricardo C <ricardo@suggestic.com>
2018-02-27 23:35:20 -0600

    add offsets function clauses

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -7,7 +7,12 @@ defmodule IslansEngine.Islands do
   def new(), do:
   %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
 
-  
+  defp offsets(:square), do: {{0,0}, {0,1}, {1,0}, {1,1}}
+  defp offsets(:atoll), do: [{0,0}, {0,1}, {1,1}, {2,0}, {2,1}]
+  defp offsets(:dor), do: [{0,0}]
+  defp offsets(:l_shape), do: [{0,0}, {1,0}, {2,0}, {2,1}]
+  defp offsets(:s_shape), do: [{0,1}, {0,2}, {1,0}, {1,1}]
+  defp offsets(_), do: {:error, :invalid_island_type}
   
 end
   
