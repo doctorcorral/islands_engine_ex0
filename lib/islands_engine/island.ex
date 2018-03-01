90601851aea4f372a13f4527fb20c57ce67611da
Ricardo C <ricardo@suggestic.com>
2018-03-01 13:00:03 -0600

    remove comments

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -4,9 +4,6 @@ defmodule IslandsEngine.Island do
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-#  def new(), do:
-#  %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
-
   def new(type, %Coordinate{} = upper_left) do
     with [_|_] = offsets <- offsets(type),
 	 %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
