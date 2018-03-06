fe249953ea1be17edf9a5627e3c38717d6a9153d
Ricardo C <ricardo@suggestic.com>
2018-03-05 18:29:16 -0600

    add types()

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -4,6 +4,8 @@ defmodule IslandsEngine.Island do
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
+  def types(), do: [:atoll, :dot, :l_shape, :s_shape, :square]
+  
   def new(type, %Coordinate{} = upper_left) do
     with [_|_] = offsets <- offsets(type),
 	 %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
