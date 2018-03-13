e4ff470c54df368f9c27316adc63b1fb79a545cd
Ricardo C <ricardo@suggestic.com>
2018-03-12 19:18:20 -0600

    add check()

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -2,6 +2,8 @@ defmodule IslandsEngine.Rules do
   alias __MODULE__
   defstruct state: :initialized
 
-  def new(), do %Rules{}
-    
+  def new(), do: %Rules{}
+
+  def check(_state, _action), do: :error
+  
 end
