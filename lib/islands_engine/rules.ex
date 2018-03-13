5d546a6849cee1361ba54c62a5b0451b4061c6aa
Ricardo C <ricardo@suggestic.com>
2018-03-12 19:54:14 -0600

    add check() clause for :position_islands

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -6,6 +6,10 @@ defmodule IslandsEngine.Rules do
 
   def check(%Rules{state: :initialized} = rules, :add_player), do:
   {:ok, %Rules{rules | state: :players_set}}
+
+  def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
+    {:ok, rules}
+  end
   
   def check(_state, _action), do: :error
   
