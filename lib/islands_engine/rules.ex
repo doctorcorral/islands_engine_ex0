9ad1585926fbd96f2bca0b1ae22cd6cc7708c7cb
Ricardo C <ricardo@suggestic.com>
2018-03-16 19:18:02 -0600

    add check() clause with :win_check

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -29,6 +29,13 @@ defmodule IslandsEngine.Rules do
 
   def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}), do:
   {:ok, %Rules{rules | state: :player2_turn}}
+
+  def check(%Rules{state: :player1_turn} = rules, {:win_check, win_or_not}) do
+    case win_or_not do
+      :no_win -> {:ok, rules}
+      :win -> {:ok, %Rules{rules | state: :game_over}}
+    end
+  end
   
   def check(_state, _action), do: :error
   
