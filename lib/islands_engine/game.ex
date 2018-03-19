5d7fa7539dfc2d50352b119fb5cf47c9778939e2
Ricardo C <ricardo@suggestic.com>
2018-03-19 16:04:55 -0600

    add GenServer demo functionallity

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,3 +1,12 @@
 defmodule IslandsEngine.Game do
   use GenServer
+
+  def handle_call(:demo_call, _from, state) do
+    {:reply, state, state}
+  end
+  
+  def handle_info(:first, state) do
+    IO.puts "This message has been handled by handle_info/2, matching on :first"
+    {:noreply, state}
+  end
 end
