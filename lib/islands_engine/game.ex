6a7d2361ec7fbc9df537fd790749e24ee7106ff1
Ricardo C <ricardo@suggestic.com>
2018-03-24 22:31:15 -0600

    add demo_call()

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -4,6 +4,10 @@ defmodule IslandsEngine.Game do
   def handle_call(:demo_call, _from, state) do
     {:reply, state, %{state | test: state.test <> "|"}}
   end
+
+  def demo_call(game) do
+    GenServer.call(game, :demo_call)
+  end
   
   def handle_info(:first, state) do
     IO.puts "This message has been handled by handle_info/2, matching on :first"
