66bf3a8890705fb39d8a459e7f2c1edcb0624fec
Ricardo C <ricardo@suggestic.com>
2018-03-25 11:34:42 -0600

    add demo callback functions

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -5,8 +5,16 @@ defmodule IslandsEngine.Game do
     {:reply, state, %{state | test: state.test <> "|"}}
   end
 
+  def handle_cast({:demo_cast, new_value}, state) do
+    {:noreply, Map.put(state, :test, new_value)}
+  end
+  
   def demo_call(game) do
     GenServer.call(game, :demo_call)
+  end
+
+  def demo_cast(pid, new_value) do
+    GenServer.cast(pid, {:demo_cast, new_value})
   end
   
   def handle_info(:first, state) do
