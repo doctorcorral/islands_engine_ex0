2f517b58d2ebbf14ea18897875ac89ca5437b4fc
Ricardo C <ricardo@suggestic.com>
2018-03-25 11:50:11 -0600

    add init()

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -4,4 +4,11 @@ defmodule IslandsEngine.Game do
 
   def start_link(name) when is_binary(name), do:
   GenServer.start_link(__MODULE__, name, [])
+
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil,  board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+  end
+  
 end
