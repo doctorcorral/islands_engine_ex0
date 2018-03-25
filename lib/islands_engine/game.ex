25bbc004cfe96c6997504b2b48480a77b779e6bb
Ricardo C <ricardo@suggestic.com>
2018-03-25 11:47:56 -0600

    add start_link()

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,5 +1,7 @@
 defmodule IslandsEngine.Game do
   use GenServer
+  alias IslandsEngine.{Board, Guesses, Rules}
 
-  
+  def start_link(name) when is_binary(name), do:
+  GenServer.start_link(__MODULE__, name, [])
 end
