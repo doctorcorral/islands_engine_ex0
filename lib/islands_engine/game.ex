def35a085081ace1c1ae3128b6d39ae5b2fa21f0
Ricardo C <ricardo@suggestic.com>
2018-04-19 12:39:51 -0500

    fix typo in handle_call({:guess_coordinate,...})

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -83,7 +83,7 @@ defmodule IslandsEngine.Game do
     with {:ok, rules} <- Rules.check(state_data.rules, {:guess_coordinate, player_key}),
 	 {:ok, coordinate} <- Coordinate.new(row, col),
 	 {hit_or_miss, forested_island, win_status, opponent_board} <-
-    Board.guess(opponen_board, coordinate),
+    Board.guess(opponent_board, coordinate),
 	 {:ok, rules} <- Rules.check(rules, {:win_checkm, win_status})
       do
       state_data
