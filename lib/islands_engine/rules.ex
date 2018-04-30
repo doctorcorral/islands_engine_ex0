1a6451652bbd33c72723cbbcd2c981ed4ff5a321
Ricardo C <ricardo@suggestic.com>
2018-04-29 20:48:14 -0500

    refactor with mix format

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -3,17 +3,18 @@ defmodule IslandsEngine.Rules do
   Islands game rule checking functions
   """
   alias __MODULE__
+
   defstruct state: :initialized,
-    player1: :islands_not_set,
-    player2: :islands_not_set
+            player1: :islands_not_set,
+            player2: :islands_not_set
 
   def new(), do: %Rules{}
 
-  defp both_players_islands_set?(rules), do:
-  rules.player1 == :islands_set && rules.player2 == :islands_set
-  
-  def check(%Rules{state: :initialized} = rules, :add_player), do:
-  {:ok, %Rules{rules | state: :players_set}}
+  defp both_players_islands_set?(rules),
+    do: rules.player1 == :islands_set && rules.player2 == :islands_set
+
+  def check(%Rules{state: :initialized} = rules, :add_player),
+    do: {:ok, %Rules{rules | state: :players_set}}
 
   def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
     case Map.fetch!(rules, player) do
@@ -24,18 +25,19 @@ defmodule IslandsEngine.Rules do
 
   def check(%Rules{state: :players_set} = rules, {:set_islands, player}) do
     rules = Map.put(rules, player, :islands_set)
+
     case both_players_islands_set?(rules) do
       true -> {:ok, %Rules{rules | state: :player1_turn}}
       false -> {:ok, rules}
     end
   end
 
-  def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}), do:
-  {:ok, %Rules{rules | state: :player2_turn}}
+  def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}),
+    do: {:ok, %Rules{rules | state: :player2_turn}}
 
-  def check(%Rules{state: :player2_turn} = rules, {:guess_coordinate, :player2}), do:
-  {:ok, %Rules{rules | state: :player2_turn}}
-  
+  def check(%Rules{state: :player2_turn} = rules, {:guess_coordinate, :player2}),
+    do: {:ok, %Rules{rules | state: :player2_turn}}
+
   def check(%Rules{state: :player1_turn} = rules, {:win_check, win_or_not}) do
     case win_or_not do
       :no_win -> {:ok, rules}
@@ -49,7 +51,6 @@ defmodule IslandsEngine.Rules do
       :win -> {:ok, %Rules{rules | state: :game_over}}
     end
   end
-    
+
   def check(_state, _action), do: :error
-  
 end
