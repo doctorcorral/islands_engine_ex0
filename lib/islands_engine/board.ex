6292646ebe87b8d14db7ab4d90b78c71448959dc
Ricardo C <ricardo@suggestic.com>
2018-04-29 20:44:43 -0500

    refactor with mix format

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -3,7 +3,7 @@ defmodule IslandsEngine.Board do
   Board related functions
   """
   alias IslandsEngine.{Coordinate, Island}
-  
+
   def new(), do: %{}
 
   def position_island(board, key, %Island{} = island) do
@@ -15,11 +15,11 @@ defmodule IslandsEngine.Board do
 
   defp overlaps_existing_island?(board, new_key, new_island) do
     Enum.any?(board, fn {key, island} ->
-    key != new_key and Island.overlaps?(island, new_island) end)
+      key != new_key and Island.overlaps?(island, new_island)
+    end)
   end
 
-  def all_islands_positioned?(board), do:
-  Enum.all?(Island.types, &(Map.has_key?(board, &1)))
+  def all_islands_positioned?(board), do: Enum.all?(Island.types(), &Map.has_key?(board, &1))
 
   def guess(board, %Coordinate{} = coordinate) do
     board
@@ -28,10 +28,10 @@ defmodule IslandsEngine.Board do
   end
 
   defp check_all_islands(board, coordinate) do
-    Enum.find_value(board, :miss, fn{key, island} ->
+    Enum.find_value(board, :miss, fn {key, island} ->
       case Island.guess(island, coordinate) do
-	{:hit, island} -> {key, island}
-	:miss -> false
+        {:hit, island} -> {key, island}
+        :miss -> false
       end
     end)
   end
@@ -40,6 +40,7 @@ defmodule IslandsEngine.Board do
     board = %{board | key => island}
     {:hit, forest_check(board, key), win_check(board), board}
   end
+
   defp guess_response(:miss, board), do: {:miss, :none, :no_win, board}
 
   defp forest_check(board, key) do
@@ -62,7 +63,6 @@ defmodule IslandsEngine.Board do
     end
   end
 
-  defp all_forested?(board), do:
-  Enum.all?(board, fn {_key, island} -> Island.forested?(island) end)
-  
+  defp all_forested?(board),
+    do: Enum.all?(board, fn {_key, island} -> Island.forested?(island) end)
 end
