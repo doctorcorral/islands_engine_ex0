20d31d0a814d15cdfef1c5752dc1d0b283298540
Ricardo C <ricardo@suggestic.com>
2018-05-05 20:05:02 -0500

    fix code

--- lib/islands_engine/game_supervisor.ex
+++ lib/islands_engine/game_supervisor.ex
@@ -1,1 +1,15 @@
-def start_link(_options), do: Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+defmodule IslandsEngine.GameSupervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: :game_supervisor)
+  end
+
+  def init([]) do
+    children = [
+      worker(IslandsEngine.Game, [], restart: :transient)
+    ]
+
+    supervise(children, strategy: :simple_one_for_one)
+  end
+end
