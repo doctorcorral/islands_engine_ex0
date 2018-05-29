2fefac398bbc37e724ca931fddb2f5f2203160bb
Ricardo C <ricardo@suggestic.com>
2018-05-29 17:29:14 -0500

    add :xprof dep

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule IslandsEngine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.8.8", only: [:dev], runtime: false}
+      {:credo, "~> 0.8.8", only: [:dev], runtime: false},
+      {:xprof, "~> 1.3.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
